b7722d4b28b215cabe417c62894248bea8b59a29
Evgen <zhe312@mail.ru>
2023-04-13 13:37:24 +0300

    task_13 solved

---
+++ task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Task(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int num = Task("Введите трехзначное число: ");
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Введено не трехзначное число. Повторите попытку ввода.");
+    return;
+}
+
+int secondNum = num / 10 % 10;
+Console.Write($"Вторая цифра числа {num} - {secondNum}");
---
+++ task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Task(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int num = Task("Введите число: ");
+if (num < 100 && num > -100)
+{
+    Console.WriteLine("В введенном числе нет третьей цифры");
+    return;
+}
+
+int thirdNum = num / 10 % 10;
+Console.Write($"Третья цифра числа {thirdNum}");
