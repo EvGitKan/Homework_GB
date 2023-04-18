1f5c1e699798c315b205ceafc76136964132b9bf
Evgen <zhe312@mail.ru>
2023-04-18 16:02:39 +0300

    add resolved task25

---
+++ task_25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numberA = InputNumber("Введите число A: ");
+int numberB = InputNumber("Введите число B: ");
+
+int numInDegree = raiseToDegree(numberA, numberB);
+Console.Write(numInDegree);
+
+int InputNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int raiseToDegree(int numA, int numB)
+{
+    int result = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+// Ниже описан метод для решения задачи не через цикл:
+// int raiseToDegree(int numA, int numB)
+// {
+//     int result = Convert.ToInt32(Math.Pow(numA, numB));
+//     return result;
+// }
