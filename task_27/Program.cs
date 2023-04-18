93fc2c49b210b8c2ca3604bdee0142a84cd98303
Evgen <zhe312@mail.ru>
2023-04-18 17:02:09 +0300

    add resolved task27

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = InputNumber("Введите число: ");
+int sum = SumDigitalsNumber(number);
+
+Console.Write($"Сумма цифр в введеном числе равна {sum}");
+
+int InputNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+int SumDigitalsNumber(int num)
+{
+    int result = 0;
+    for (; num != 0; num /= 10)
+    {
+        result = result + num % 10;
+    }
+    return result;
+}
