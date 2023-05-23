735be4afe0e812638d1a6f37499557d1932df9d3
Evgen <zhe312@mail.ru>
2023-05-23 10:50:05 +0300

    add resolved task62

---
+++ task_62/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+//Генерация  двумерного массива.
+int[,] GenerateMatrixIntNum(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    int count = 1;
+    int i = 0;
+    int j = 0;
+
+    while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = count;
+        count++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;
+}
+
+//Вывод массива в консоль.
+void PrintMatrixIntNum(int[,] newMatrix)
+{
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{newMatrix[i, j],2} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array = GenerateMatrixIntNum(4, 4);
+PrintMatrixIntNum(array);
